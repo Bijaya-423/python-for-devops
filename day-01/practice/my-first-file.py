616b28f59ecced05f82f641dc2fdf66f68b48f17
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-01 22:58:29 +0530

    added

---
+++ day-01/practice/my-first-file.py
@@ -0,0 +1,19 @@
+print("Hello, World!") 
+
+
+#Script - set of instructions
+#variable & constant
+
+
+#vary + able 
+#yes = constant
+# 1 = constant
+#100 = constant
+
+
+output = "yes"
+output = "no"
+
+a = 100
+b = 200
+print("Hello Dosto")
