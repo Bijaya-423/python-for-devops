e4b4813de8aadf56edef365d8cdb5f82b9d45afb
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-02 08:42:54 +0530

    added

--- day-01/practice/my-first-day.py
+++ day-01/practice/my-first-day.py
@@ -15,9 +15,65 @@ print("This is my first day of learning Python.")
 
 output = "yes" # variable
 output = "no"
-
+# = is an assignment operator
 
 a = 100
 b = 200
 env = "testing, staging, production, development"
 print("Hello dosto")
+print(a+b)
+# + , -, *, /, //, **, % (arithmetic operators)
+# (a+b) -> a , b are operands 
+# a + b -> operations
+
+
+x = 50
+y = 20
+
+z = x * y
+print("the multiplication of x and y is :-", z)
+
+
+
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+
+addition = x + y
+print("The addition of x and y is:-", addition)
+
+subtraction = x - y
+print("The subtraction of x and y is:-", subtraction)
+
+multiplication = x * y
+print("The multiplication of x and y is:-", multiplication)
+
+division = x / y
+print("The division of x and y is:-", division)
+
+modulus = x % y
+print("The modulus of x and y is:-", modulus)
+
+floor_division = x // y
+print("The floor division of x and y is:-", floor_division)
+
+
+#data types
+# int - 1, 2, 3, 4, 5
+# float - 1.5, 2.4, -1.4
+# bool - True, False
+# str - "Hello", "world"
+print(type(x))
+print(type(y))
+
+
+
+x = "Hello, World!"
+y = 100
+z = 3.14
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(f"The concatenation of x and y is:-{x}+{y}")
+
+print(y + z)
