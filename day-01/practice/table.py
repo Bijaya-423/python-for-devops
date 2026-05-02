745e582ed1a45b23d57a908de7b4d8eaff2c8d7e
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-02 20:13:40 +0530

    added

--- day-01/practice/table.py
+++ day-01/practice/table.py
@@ -1,6 +1,25 @@
-num = int(input("Enter a number:-"))
+# num = int(input("Enter a number:-"))
 
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num * i}")
 
-    
+# name = input("Enter your name:-")
+
+# print(f"Hello dosto, {name} kaise ho?")
+
+
+
+# suraj = "chand"
+# while suraj == "chand":
+#     name = input("Enter your name:-")
+#     print(f"Hello dosto, {name} kaise ho?")
+#     break
+
+#real world
+choice = input("Enter your choice (press q to quit): ")
+
+while choice != "q":
+    num = int(input("Enter a number you want the table for:-"))
+
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
