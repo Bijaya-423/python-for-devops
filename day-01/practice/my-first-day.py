8ce31d848965f66c3fe4adc8a47db9a0a5c1c7de
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-02 08:08:18 +0530

    added my practice code

--- day-01/practice/my-first-day.py
+++ day-01/practice/my-first-day.py
@@ -1,1 +1,16 @@
 print("Hello, World!")
+ 
+
+
+#Script - set of instructions
+print("This is my first day of learning Python.")
+
+#variable & constant
+
+#vary + able
+#yes = constant
+#  1 = constant
+# 100 = constant
+output = "yes"
+output = "no"
+print("Hello dosto")
