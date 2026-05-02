778827510682f4935303e6d07efa1fcd0378786b
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-02 11:05:42 +0530

    added

--- day-01/practice/checkenv.py
+++ day-01/practice/checkenv.py
@@ -1,45 +1,45 @@
-#Get the Enviornment from user and print it
+Get the Enviornment from user and print it
 
-# env = input("Enter the env:- ")
-# print("The enviornment is:-", env)
-
-
-# a = int(input("Enter the value of a: "))
-# #Typecasting - converting one datatype to another datatype
-# print(type(a))
-# b = int(input("Enter the value of b: "))
-# print(type(b))
-
-# print("mul: ", a*b )
-# print("add: ", a+b )
-# print("sub: ", a-b )
-# print("div: ", a/b )
+env = input("Enter the env:- ")
+print("The enviornment is:-", env)
 
 
-# a = input("Enter:-")
-# print("The value of a is:-", a)
-# print(type(a))
+a = int(input("Enter the value of a: "))
+#Typecasting - converting one datatype to another datatype
+print(type(a))
+b = int(input("Enter the value of b: "))
+print(type(b))
 
-# b = input("Enter:-")
-# print("The value of b is:-", b)
-# print(type(b))
-
-# print("The addition of a and b is:-", a+b)
-# print("The multiplication of a and b is:-", a*b)
+print("mul: ", a*b )
+print("add: ", a+b )
+print("sub: ", a-b )
+print("div: ", a/b )
 
 
+a = input("Enter:-")
+print("The value of a is:-", a)
+print(type(a))
 
-# env = input("Enter the enviornment:-")
-# print("The enviornment is:-", env)
+b = input("Enter:-")
+print("The value of b is:-", b)
+print(type(b))
 
-# if env == "prod":
-#     print("Don't deploy on friday.")
+print("The addition of a and b is:-", a+b)
+print("The multiplication of a and b is:-", a*b)
 
-# elif env == "staging":
-#     print("Deploy on friday")
 
-# else:
-#     print("You can do anything.")
+
+env = input("Enter the enviornment:-")
+print("The enviornment is:-", env)
+
+if env == "prod":
+    print("Don't deploy on friday.")
+
+elif env == "staging":
+    print("Deploy on friday")
+
+else:
+    print("You can do anything.")
 
 
 
