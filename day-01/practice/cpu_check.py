99fc1219b3289bbaac2967b9ab75d5a8d8dc786f
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-08 12:04:51 +0530

    added

--- day-01/practice/cpu_check.py
+++ day-01/practice/cpu_check.py
@@ -1,32 +1,46 @@
-# you have to user input to check the CPU threshold. If the CPU usage is above the threshold, print a warning message.
+# # you have to user input to check the CPU threshold. If the CPU usage is above the threshold, print a warning message.
 
-# threshold -> means limit
+# # threshold -> means limit
 
-#check current cpu usage
+# #check current cpu usage
 
-#if the cpu usage threshold is above 80% , send the email
-import psutil #import the psutil in pypi
+# #if the cpu usage threshold is above 80% , send the email
+
+# import psutil #import the psutil in pypi
+
+# def check_cpu_usage():
+#     user_cpu = int(input("Enter the current CPU Threshold:- "))
+
+#     current_cpu = psutil.cpu_percent(interval=1) #last 1 second , per cpu - interval means
+#     # current_cpu = psutil.cpu_percent(interval=1, percpu=True) #last 1 second , per cpu - interval means
+
+
+#     print(f'Per-Core: {current_cpu}')
+
+#     if current_cpu > user_cpu:
+#         print(f"Warning: cpu usage is above {user_cpu}%")
+#         print(f"CPU Alert Email sent ....")
+#     else:
+#         print(f"cpu in safe state.")
+
+# check_cpu_usage()
+
+
+
+
+
+
+import psutil
 
 def check_cpu_usage():
-    user_cpu = int(input("Enter the current CPU Threshold:- "))
+    user_cpu = int(input("Enter the current CPU Threshold:-"))
 
-    current_cpu = psutil.cpu_percent(interval=1) #last 1 second , per cpu - interval means
-    # current_cpu = psutil.cpu_percent(interval=1, percpu=True) #last 1 second , per cpu - interval means
-
-
-    print(f'Per-Core: {current_cpu}')
+    current_cpu = psutil.cpu_percent(interval=1)
 
     if current_cpu > user_cpu:
-        print(f"Warning: cpu usage is above {user_cpu}%")
-        print(f"CPU Alert Email sent ....")
+        print(f'Warning: CPU usage is above {user_cpu}%')
+        print("CPU Alert Email Sent...")
     else:
-        print(f"cpu in safe state.")
-
+        print("Cpu in safe state.")
 check_cpu_usage()
-
-
-
-
-
-
 
