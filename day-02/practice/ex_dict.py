d6784c1ae58d6b495e9dd533e999515d1279dc3d
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-08 16:40:23 +0530

    added

--- day-02/practice/ex_dict.py
+++ day-02/practice/ex_dict.py
@@ -7,8 +7,23 @@ info = {
     "married": False, #bool
     "favorities": ["Reading", "Travelling", "Cooking"] #list
 }
-print(f"i live in {info['city']}")
-print(f"i love {info.get('favoriti', "Not found")}")
-info.update({"blood_group": "O+", "hobbies": "Playing cricket"})
-print(info)
-print(dir(info))
+# print(f"i live in {info['city']}")
+# print(f"i love {info.get('favoriti', "Not found")}")
+# info.update({"blood_group": "O+", "hobbies": "Playing cricket"})
+# print(info)
+# print(dir(info))
+for i in info:
+    print(f"{i} : {info[i]}")
+    print(i)
+    
+
+for key, value in info.items():
+    print(key, value)
+
+
+for i in info.keys():
+    print(i)
+
+for  i in info.values():
+    print(i)
+    
