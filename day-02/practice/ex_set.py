143fc6b18d069df7b6510d76b5346ab48b6e5032
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-08 16:46:52 +0530

    added

---
+++ day-02/practice/ex_set.py
@@ -0,0 +1,12 @@
+info = {1}
+print(type(info))
+
+
+days = set()
+days = {"Monday", "Tuesday", "Monday", "Tuesday", "Monday", "Tuesday"}
+print(days)
+
+
+nums = [11, 11, 11, 11, 11, 22, 22, 2, 2, 2, 333, 33, 33, 33, 33, 33]
+nums = list(set(nums))
+print(nums)
