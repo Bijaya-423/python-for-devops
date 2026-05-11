cc3645b1f01f7c4be43eb782a276c9195a0117b2
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-11 20:21:24 +0530

    added

---
+++ day-01/practice/practice_03.py
@@ -0,0 +1,54 @@
+print("multiplicatioon table")
+
+# mul = 7
+
+# n = 1
+
+# while n <= 10:
+#     print(mul * n)
+#     n += 1
+
+# for num in range(1, 21):
+#     print(f"The multiplication of {num}")
+
+#     for i in range(1, 11):
+#         print(f"{num} x {i} = {num * i}")
+
+#     print("*********************************")
+
+
+# n = int(input("Enter the number between 1 to 15:-"))
+
+# for row in range(1, n + 1):
+#     for col in range(1, n+1):
+#         print(row * col)
+#     print("------------")
+# num = 7
+# i = 1
+# while i <= 10:
+#     print(f"{num} x {i} = {num * i}")
+#     i += 1
+#     print("--")
+
+
+# num = int(input("Enter 1 to 10 any number :-"))
+# while num <= 10:
+#     i = 1
+#     while i <= 10:
+#         print(f'{num} x {i} = {num * i}')
+#         i += 1
+#     num += 1
+#     print("################")
+
+
+
+# num = int(input("Enter the number:-"))
+# col = 1
+# while col <= num:
+#     print(f'multiplication of {col}')
+
+#     row = 1
+#     while row <= 10:
+#         print(f"{col} x {row} = {col * row}")
+#         row += 1
+#     col += 1
