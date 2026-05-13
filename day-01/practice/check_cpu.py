3c2fc86d2edd6d9c05cad403d2747c50276ca28a
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-13 15:10:01 +0530

    added

--- day-01/practice/check_cpu.py
+++ day-01/practice/check_cpu.py
@@ -1,11 +1,16 @@
-import psutil # import the lib from pypi
-
 # aapko kaam karna hai ki user se CPU threshold lo
 # current cpu usage pata karo 
 # agar cpu usage threshold se zyada hua, email kar do
 
+
+
+
+
+import psutil # import the lib from pypi
+
+
 def check_cpu_threshold():
-    cpu_threshold = int(input("Enter the CPU Threshold")) # DONE
+    cpu_threshold = int(input("Enter the CPU Threshold:-")) # DONE
 
     current_cpu = psutil.cpu_percent(interval=1) # DONE
     print("Current CPU %: ",current_cpu)
