6ecb20f0b08150e1e11c4b6999ebd93ac262d812
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-13 19:20:44 +0530

    adddedd

---
+++ day-01/practice/retry_mech.py
@@ -0,0 +1,25 @@
+import requests
+import time
+
+
+url = "https://api.github.com"
+
+attempt = 1
+
+while attempt <= 5:
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            print("API is up and running!")
+            break
+    except Exception as e:
+        # print(f"Attempt {attempt} failed: {e}")
+        # attempt += 1
+        # time.sleep(2)
+        print("Error:", e)
+
+        print("Retrying in 2 seconds...")
+
+        attempt += 1
+        time.sleep(2)
