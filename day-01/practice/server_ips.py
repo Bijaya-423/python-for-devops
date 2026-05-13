956062601c2ec516b04221abf3d4e0876a525330
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-13 17:10:22 +0530

    added

---
+++ day-01/practice/server_ips.py
@@ -0,0 +1,13 @@
+print("This is server ips file")
+
+# import os
+
+# servers = ["8.8.8.8", "google.com", "github.com"]
+
+# for server in servers:
+#     response = os.system(f'ping -c 1 {server} > /dev/null 2>&1')
+
+#     if response == 0:
+#         print(f"{server} is up")
+#     else:
+#         # print(f"{server} is down")
