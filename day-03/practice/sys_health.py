02c9a7049e58d0f7946d7a5eb4ba6c18642446bd
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-16 22:36:45 +0530

    added

---
+++ day-03/practice/sys_health.py
@@ -0,0 +1,11 @@
+import psutil
+
+
+def check_system_health():
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+    print(f"CPU Usage: {cpu}%")
+    print(f"Memory Usage: {memory}%")
+    print(f"Disk Usage: {disk}%")
+check_system_health()
