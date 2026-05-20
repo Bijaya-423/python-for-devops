669105d0378ed84b611476dedeaa5ac8645191a7
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-20 08:02:06 +0530

    added

--- day-08/practice/demo_reading.py
+++ day-08/practice/demo_reading.py
@@ -1,4 +1,8 @@
-from utilities import read_file # importing the package
-# Abstraction
+# from utilities import read_file # importing the package
+# # Abstraction
+
+# print(read_file("app.log"))
+
+from utils import read_file
 
 print(read_file("app.log"))
