d5de0289b6106f1930f46bbc5ab2f9bd18aaf1b7
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-20 16:15:41 +0530

    added

--- day-08/practice/amazon_s3_utils.py
+++ day-08/practice/amazon_s3_utils.py
@@ -1,6 +1,17 @@
 import boto3
+import pdb
 
 s3_client = boto3.client("s3") #creating a client for s3 access
 
 s3 = s3_client.list_buckets()
-print(s3)
+
+# pdb.set_trace()
+for key, value in s3["Buckets"]:
+    # print(key, value)
+    if key == "Buckets":
+        print(value)
+    else:
+        pass
+
+
+
