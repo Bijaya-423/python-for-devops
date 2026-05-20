42a307c4a8cce2f1c043231942cfe080e7b74adc
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-20 08:02:47 +0530

    added

---
+++ day-08/practice/utils.py
@@ -0,0 +1,16 @@
+"packages : are python functions that can be imported in some other code file"
+
+
+import json
+
+
+
+def read_file(filename):
+    with open(filename, "r") as file:
+        return file.readlines()
+    
+def write_file(filename, json_object):
+    with open(filename, "w+") as file:
+        json.dump(json_object, file)
+
+
